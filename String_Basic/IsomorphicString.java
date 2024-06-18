c15bbfcb77b933c947a92026a7ecbea7ed071bf9
aman <30n76e@gmail.com>
2024-06-18 14:28:33 +0530

    isomorphic string

---
+++ String_Basic/IsomorphicString.java
@@ -0,0 +1,48 @@
+package String_Basic;
+
+import java.util.HashMap;
+
+/**
+ * IsomorphicString
+ */
+public class IsomorphicString {
+
+
+    public static void display(String a1,String a2){
+        if(a1.length()!=a2.length()){
+            System.out.println("Not Isomorphic");
+            return;
+        }
+        boolean isIsomorphic = true;
+        HashMap<Character,Character> hashMap = new HashMap<Character,Character>();
+
+        for(int i=0;i<a1.length();i++){
+            if(hashMap.containsKey(a1.charAt(i))){
+                if(hashMap.get(a1.charAt(i))!=a2.charAt(i)){
+                    
+                    isIsomorphic = false;
+                    break;
+                }
+
+            }else{
+                hashMap.put(a1.charAt(i), a2.charAt(i));
+            }
+            
+
+        }
+
+        if(isIsomorphic==true){
+            System.out.print("String is isomorphic");
+        }else{
+            System.out.print("Not isomorphic");
+
+        }
+
+    }
+
+    public static void main(String ags[]){
+
+        display("egg", "add");
+
+    }
+}
